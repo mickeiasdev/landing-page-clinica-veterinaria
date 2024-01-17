3203bcf03bab26830eec494626af3874338df4c7
Mickeias Coelho <130601846+mickeiasdev@users.noreply.github.com>
2024-01-17 14:20:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let elementosDuvidas = document.querySelectorAll(".duvida")
+
+elementosDuvidas.forEach( function (duvida){
+    duvida.addEventListener("click", function(){
+        duvida.classList.toggle("ativa")
+    })
+})
