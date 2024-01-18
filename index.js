d4a7da5d375e119458f23b293547381bd50a2cd8
Mickeias Coelho <130601846+mickeiasdev@users.noreply.github.com>
2024-01-18 11:20:47 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let elementosDuvidas = document.querySelectorAll(".duvida")
-
-elementosDuvidas.forEach( function (duvida){
-    duvida.addEventListener("click", function(){
-        duvida.classList.toggle("ativa")
-    })
-})
